76431a9326c9a68d9f345c1f366dc389a1d250dc
Tim Bezhashvyly <tim@openstream.ch>
2013-01-19 12:34:05 +0100

    completed admin interface

--- app/code/community/HackathonWebsite/ProjectIdea/Block/Adminhtml/ProjectIdea.php
+++ app/code/community/HackathonWebsite/ProjectIdea/Block/Adminhtml/ProjectIdea.php
@@ -5,10 +5,12 @@ class HackathonWebsite_ProjectIdea_Block_Adminhtml_ProjectIdea extends Mage_Admi
 
     public function __construct()
     {
-        parent::__construct();
         $this->_controller = 'adminhtml_projectIdea';
         $this->_blockGroup = 'hackathonwebsite_projectidea';
-        $this->_headerText = Mage::helper('hackathonwebsite_projectidea')->__('Project Ideas');
+        $this->_headerText = $this->__('Project Ideas');
+        $this->_addButtonLabel = $this->__('Add Idea');
+
+        parent::__construct();
     }
 
 }
