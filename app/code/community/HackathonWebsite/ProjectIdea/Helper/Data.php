fb2664bde371b4fcda38c8cd2affabe2129289ae
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2013-06-01 20:05:20 +0200

    added a check if there are no ids in the session

--- app/code/community/HackathonWebsite/ProjectIdea/Helper/Data.php
+++ app/code/community/HackathonWebsite/ProjectIdea/Helper/Data.php
@@ -11,9 +11,12 @@ class HackathonWebsite_ProjectIdea_Helper_Data extends Mage_Core_Helper_Abstract
     public function isCustomerOwnerOfProjectIdea(HackathonWebsite_ProjectIdea_Model_ProjectIdea $projectIdea)
     {
         $projectIdeaIds = Mage::getSingleton('customer/session')->getProjectIdeaIds();
-        $isInSession = array_key_exists($projectIdea->getId(), $projectIdeaIds);
         $customerOwnsIdea = $projectIdea->getCustomerId() !== null
             && ($projectIdea->getCustomerId() == Mage::getSingleton('customer/session')->getCustomerId());
+        if(!is_array($projectIdeaIds)) {
+            return $customerOwnsIdea;
+        }
+        $isInSession = array_key_exists($projectIdea->getId(), $projectIdeaIds);
         return $isInSession || $customerOwnsIdea;
     }
 }
