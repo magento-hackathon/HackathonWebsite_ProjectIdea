f8c0ed0dc1852907ec43e102cd224dc18c2ea52b
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2013-06-01 15:00:44 +0200

    added possibility to change projectIDea

--- app/code/community/HackathonWebsite/ProjectIdea/Block/Form.php
+++ app/code/community/HackathonWebsite/ProjectIdea/Block/Form.php
@@ -1,5 +1,24 @@
 <?php
+/**
+ * Class HackathonWebsite_ProjectIdea_Block_Form
+ *
+ * @method int getProjectIdeaId
+ */
 class HackathonWebsite_ProjectIdea_Block_Form extends Mage_Core_Block_Template
 {
+    protected $_projectIdea;
 
+    public function getProjectIdea()
+    {
+        if (!$this->_projectIdea) {
+            $this->_projectIdea = Mage::getModel('hackathonwebsite_projectidea/projectIdea')->load(
+                $this->getProjectIdeaId()
+            );
+        }
+        // if the user doesn't own the idea, reset it
+        if (!Mage::helper('hackathonwebsite_projectidea')->isCustomerOwnerOfProjectIdea($this->_projectIdea)) {
+            $this->_projectIdea = Mage::getModel('hackathonwebsite_projectidea/projectIdea');
+        }
+        return $this->_projectIdea;
+    }
 }
